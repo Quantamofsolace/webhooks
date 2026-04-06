7cff3d61d3abd4bede2bf154377cbc67583ca3a0
Quantamofsolace <rameshmanipatruni11@gmail.com>
2026-04-06 10:49:05 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  description = "This variable is used to specify the AMI ID for the EC2 instance."
+  default     = ""
+  type        = string
+
+
+}
+variable "instance_type" {
+  description = "This variable is used to specify the instance type for the EC2 instance."
+  default     = ""
+  type        = string
+
+}
